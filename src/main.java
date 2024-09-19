daa29b620517ac20e661f9535831083d32109a28
MaksimKosarevHSE <maksimkosarev96@gmail.com>
2024-09-19 17:11:31 +0300

    Update from Server

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
+        People p10;
         People p1 = new People(1, "Maksim", 18);
         System.out.println(p1 + "He");
     }
